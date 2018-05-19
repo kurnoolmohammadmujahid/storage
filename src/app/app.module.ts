74de82cb4bc507d143ef1d26071d50dc71985698
K Mohammad Mujahid <mujahidkm828@gmail.com>
2018-05-19 17:59:14 +0530

    Seventh Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
+    Pro,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private pro: Pro, public navCtrl: NavController, private iab: InAppBrowser) {
 
+    // this.pro.getAppInfo().then((res: AppInfo) => {
+    //   console.log(res)
+    // })
+    
+    // // Get live update info
+    // this.pro.deploy.info().then((res: DeployInfo) => {
+    //   console.log(res)
+    // })
+
+  }
+
+  web(){
+    this.iab.create('https://ionicframework.com/');
   }
 
 }
